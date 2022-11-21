2fda2108127dd43d152a3fa4285bd33b428299f7
SnehaMahadeva <116667567+SnehaMahadeva@users.noreply.github.com>
2022-11-21 15:27:20 +0530

    Update HelloNew.java

--- HelloNew.java
+++ HelloNew.java
@@ -7,7 +7,7 @@ Sample Output:
 Hello World
 
 Hello World*/
-public class HellNew{
+public class HelloNew{
     public static void main(String []args){
         System.out.println("\n Hello World \n");
         System.out.println("Hello World");
