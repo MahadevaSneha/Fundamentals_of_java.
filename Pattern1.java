920314b156bca830d49dafa5dbf06b51ca0ae55f
SnehaMahadeva <116667567+SnehaMahadeva@users.noreply.github.com>
2022-12-01 09:11:32 +0530

    Pattern1.java

---
+++ Pattern1.java
@@ -0,0 +1,42 @@
+/*Pattern I
+Requested files: Pattern1.java (Download)
+Maximum number of files: 5
+Type of work: Individual work
+Write a Python Program to solve the pattern in dynamic.
+
+Sample Input 1:
+5
+Sample Output 1:
+*****
+*****
+*****
+*****
+*****
+Sample Input 2:
+7
+Sample Output 2:
+*******
+*******
+*******
+*******
+*******
+*******
+*******
+*/
+import java.util.Scanner;
+public class Pattern1
+{
+    public static void main(String[] args)
+    {
+       Scanner sc=new Scanner(System.in);
+       int n=sc.nextInt();
+       for(int i=1;i<=n;i++)
+       {
+           for(int j=1;j<=n;j++)
+           {
+           System.out.printf("*");
+           }
+           System.out.printf("\n");
+       }
+    }
+}
