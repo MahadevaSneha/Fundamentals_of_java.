52657c3a6b2fd4d5f0ced7e47e22d5b7912e7f6d
SnehaMahadeva <116667567+SnehaMahadeva@users.noreply.github.com>
2023-02-13 17:23:33 +0530

    duble.java

---
+++ duble.java
@@ -0,0 +1,24 @@
+/*Double the number
+Requested files: duble.java (Download)
+Maximum number of files: 5
+Type of work: Individual work
+You need to double the integer and return it.
+
+Input:
+
+2
+
+output:
+
+4
+*/
+import java.util.Scanner;
+class duble
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.printf("%d",2*n);
+    }
+}
