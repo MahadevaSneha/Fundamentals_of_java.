bcd2c50d5e000efd285e7ab043a56a8421c053d4
SnehaMahadeva <116667567+SnehaMahadeva@users.noreply.github.com>
2023-02-13 17:33:48 +0530

    Array1.java

---
+++ Array1.java
@@ -0,0 +1,21 @@
+/*Array List- Strings
+Requested files: Array.java (Download)
+Maximum number of files: 5
+Type of work: Individual work
+Write a java program using ArrayList ti print the following output:
+
+ArrayList: [Java, Python, Swift]
+*/
+import java.util.*;
+class Array
+{
+    public static void main(String args[])
+    {
+        ArrayList<String> a=new ArrayList<String>();
+        a.add("Java");
+        a.add("Python");
+        a.add("Swift");
+        System.out.println("ArrayList: "+a);
+        
+    }
+}
