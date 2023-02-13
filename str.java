d2838eb844bebff91b325c9218a6bb2a959ed194
SnehaMahadeva <116667567+SnehaMahadeva@users.noreply.github.com>
2023-02-13 17:21:25 +0530

    str.java

---
+++ str.java
@@ -0,0 +1,16 @@
+/*Returning Strings
+Requested files: str.java (Download)
+Maximum number of files: 5
+Type of work: Individual work
+Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+*/
+import java.util.*;
+public class str{
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+String a=sc.next();
+System.out.println("Hello,"+a+" how are you doing today?");
+}
+}
+
+     
