5f22a5e3850fbcdae7e423f9244a2fd8ad031202
SnehaMahadeva <116667567+SnehaMahadeva@users.noreply.github.com>
2023-02-13 13:45:36 +0530

    uppercase1.java

---
+++ uppercase1.java
@@ -0,0 +1,24 @@
+/* converting Lower case string to uppercase string L2
+Requested files: uppercase.java (Download)
+Type of work: Individual work
+Write a program that accepts sequence of lines as input and prints the lines after making all characters in the sentence capitalized.
+
+input
+
+Hello world
+
+output
+
+
+HELLO WORLD*/
+import java.util.Scanner;
+public class uppercase
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        String s=str.toUpperCase();
+        System.out.println(s);
+    }
+}
